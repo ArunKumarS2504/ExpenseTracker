7f33ed7207930d1b49e133f4dc7dd9d9accc89b2
Hrithik Arun Kumar M <128972805+Hrithik0216@users.noreply.github.com>
2024-05-06 16:03:15 +0530

    first commit

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='bg-dark text-light p-2'>
+            <h6 className='text-center'>Hrithik Arun Kumar M </h6>
+        </div>
+    )
+}
+
+export default Footer
